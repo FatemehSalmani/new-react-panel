1b8b7cf7e377f68d0c80db3d6ae57b1d197785d6
Fatemeh <fatima27579@gmail.com>
2020-02-21 00:24:09 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import NavBar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
   );
 }
 
