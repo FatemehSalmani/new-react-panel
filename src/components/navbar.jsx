cf3e9ea0f752d16a5ddf1c444c644683e1759a37
faslm <fatemeh.salmani.4033@gmail.com>
2020-02-20 10:30:24 -0800

    added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function navbar() {
+  return (
+    
+      <nav className="navbar navbar-light bg-light">
+             <span className="navbar-brand" >
+                 navbar part
+            </span>
+            </nav>
+  );
+}
+
+export default navbar;
