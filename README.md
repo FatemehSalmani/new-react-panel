f03645915fa3044dd5ec78bdeebd8744559a4e3d
faslm <fatemeh.salmani.4033@gmail.com>
2020-02-20 09:57:46 -0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-﻿# React Panel
-A simple react project to improve my web development skills
-
-
-![build react-panel](https://github.com/Gammate/form_pwa/workflows/pwa/badge.svg)
---
+++ precache-manifest.5d25b41ab7d6c40f53b17d83ae000f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67d3c2f391dae5d57f35a215ff69dd9",
+    "url": "/new-react-panel/index.html"
+  },
+  {
+    "revision": "ce942ef169d84d84f5f5",
+    "url": "/new-react-panel/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "67108f48db802aeaec95",
+    "url": "/new-react-panel/static/js/2.ce179c5f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/new-react-panel/static/js/2.ce179c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce942ef169d84d84f5f5",
+    "url": "/new-react-panel/static/js/main.a597dc77.chunk.js"
+  },
+  {
+    "revision": "846353f6b6fd728de47b",
+    "url": "/new-react-panel/static/js/runtime-main.d11b1c94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-react-panel/static/media/logo.5d5d9eef.svg"
+  }
+]);
