cc76312e626d77f09c54025250c4d90a7ed0fb46
Fatemeh <fatima27579@gmail.com>
2020-02-21 00:05:08 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from './components/navbar';
-import SideBar from '/components/sidebar';
+import SideBar from './components/sidebar';
 
 function App() {
   return (
