752420c4571c90f19f026f46f4e7961e380b446d
faslm <fatemeh.salmani.4033@gmail.com>
2020-02-24 23:41:34 -0800

    updated sidebar

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,18 +1,20 @@
 import React from "react"
+import "./componentStyles.css"
 import NavBar from'./navbar';
 import SideBar from './sidebar';
+
 function Template({ children }) {
-    return (
-      
-   <div class="container">
-        <NavBar/>
-        <SideBar/>
-        <div class="row">
-    <div id="content" class="d-inline m-lg-2 col-lg-8 col-md-9 col-sm-6 col-xs-12">
+    return (   
+   <div class="container ">
+        <NavBar/> 
+         <div class="row">
+           <div id="content" class="col-9 float-left">
                 {children}
-     </div>
-    </div>
-     
+           </div>
+           <div id="sidebar" class="col-3 m-1 bg-dark text-white-50 float-right " >
+           <SideBar/>
+           </div>
+        </div>
   </div>
     
   )
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,12 +1,11 @@
 import React from "react"
-import "./componentStyles.css"
+
 const Sidebar = () => {
     return(
     
-        <div class=" d-inline-block m-1 bg-dark text-white-50 pull-right" id="sidebar" >
+       
             <h3>Sidebar</h3>
-        </div>
-
+       
    
     )
 }
