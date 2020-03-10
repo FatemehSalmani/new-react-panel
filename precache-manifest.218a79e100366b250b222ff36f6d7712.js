5c1cf4b49530fee94548c9c1666338e6f896adce
faslm <fatima27579@gmail.com>
2020-03-10 20:04:20 +0000

    Deploying to gh-pages from  - 65e731e1a0286c2d62b1d5ed08fa515f18999709 🚀

---
+++ precache-manifest.218a79e100366b250b222ff36f6d7712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c756403ee921f9847d968027301c67c",
+    "url": "/new-react-panel/index.html"
+  },
+  {
+    "revision": "d860008be176c03ece91",
+    "url": "/new-react-panel/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ae4797e76c2529660c56",
+    "url": "/new-react-panel/static/css/main.cdcc70c1.chunk.css"
+  },
+  {
+    "revision": "d860008be176c03ece91",
+    "url": "/new-react-panel/static/js/2.611c361c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/new-react-panel/static/js/2.611c361c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4797e76c2529660c56",
+    "url": "/new-react-panel/static/js/main.4636094f.chunk.js"
+  },
+  {
+    "revision": "846353f6b6fd728de47b",
+    "url": "/new-react-panel/static/js/runtime-main.d11b1c94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/new-react-panel/precache-manifest.0a5a6cf0a0ea14b0998887d872d3d6c5.js"
+  "/new-react-panel/precache-manifest.218a79e100366b250b222ff36f6d7712.js"
 );
 
 self.addEventListener('message', (event) => {
