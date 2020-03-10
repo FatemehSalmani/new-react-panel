daa618425618618cdde0eb4f965a446e7e8f0da7
faslm <fatima27579@gmail.com>
2020-03-10 20:06:16 +0000

    Deploying to gh-pages from  - 352ff2527782289c983fa8fb19022512ee701ea9 🚀

---
+++ precache-manifest.110ef37d513e1f6e9c6e10a6b3081780.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e110749602b2f6bdd50312a80d50122d",
+    "url": "/new-react-panel/index.html"
+  },
+  {
+    "revision": "d860008be176c03ece91",
+    "url": "/new-react-panel/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "d9161709fcf779e7284f",
+    "url": "/new-react-panel/static/css/main.cdcc70c1.chunk.css"
+  },
+  {
+    "revision": "d860008be176c03ece91",
+    "url": "/new-react-panel/static/js/2.611c361c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/new-react-panel/static/js/2.611c361c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9161709fcf779e7284f",
+    "url": "/new-react-panel/static/js/main.5e6f1989.chunk.js"
+  },
+  {
+    "revision": "846353f6b6fd728de47b",
+    "url": "/new-react-panel/static/js/runtime-main.d11b1c94.js"
+  }
+]);
